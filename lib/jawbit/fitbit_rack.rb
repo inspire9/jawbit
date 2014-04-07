e1d3459cf0ee2af47e940ca51474c365409ad159
Pat Allan <pat@freelancing-gods.com>
2014-04-07 20:21:20 +1000

    All the debugging information. Hack.

--- lib/jawbit/fitbit_rack.rb
+++ lib/jawbit/fitbit_rack.rb
@@ -18,6 +18,10 @@ class Jawbit::FitbitRack
   attr_reader :subscriber_id, :consumer_secret
 
   def json(request)
-    MultiJson.load request.params['updates']
+    Rails.logger.info request.params['updates'].class.name
+    Rails.logger.info request.params['updates'].path
+    Rails.logger.info request.params['updates']['tempfile'].read
+
+    MultiJson.load request.params['updates']['tempfile'].read
   end
 end
